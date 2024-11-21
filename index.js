0c4f05539f1fe91af55fe45bc22d894688f488b3
Artur Sultanov <artur_sultanov@sme-soft.by>
2024-11-21 15:24:32 +0300

    added: responsive header

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
+// toggle menu
+const headerMenu = document.getElementById('menu');
+const expandedMenu = document.querySelector('.expanded-menu');
+const menuIcon = headerMenu.querySelector('img');
+
+headerMenu.addEventListener('click', () => {
+    expandedMenu.classList.toggle('open');
+
+    if (expandedMenu.classList.contains('open')) {
+        menuIcon.src = 'assets/icons/close mobile.svg';
+    } else {
+        menuIcon.src = 'assets/header-menu.svg';
+    }
+});
 // Animated landing
 const texts = document.querySelectorAll('.animative-text .text-block');
 const ctaBlock = document.querySelector('.animative-text .cta-block');
