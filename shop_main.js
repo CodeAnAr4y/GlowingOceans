529857cc4d69b924e836b453f5f5fa450855564b
Artur Sultanov <artur_sultanov@sme-soft.by>
2024-11-29 14:11:41 +0300

    non-adaptive shop main page

--- shop_main.js
+++ shop_main.js
@@ -1,35 +1,35 @@
 // toggle menu
-const headerMenu = document.getElementById('menu');
-const expandedMenu = document.querySelector('.expanded-menu');
-const menuIcon = headerMenu.querySelector('img');
+const headerMenu = document.getElementById("menu");
+const expandedMenu = document.querySelector(".expanded-menu");
+const menuIcon = headerMenu.querySelector("img");
 
-headerMenu.addEventListener('click', () => {
-    expandedMenu.classList.toggle('open');
+headerMenu.addEventListener("click", () => {
+    expandedMenu.classList.toggle("open");
 
-    if (expandedMenu.classList.contains('open')) {
-        menuIcon.src = 'assets/icons/close mobile.svg';
+    if (expandedMenu.classList.contains("open")) {
+        menuIcon.src = "assets/icons/close mobile.svg";
     } else {
-        menuIcon.src = 'assets/header-menu.svg';
+        menuIcon.src = "assets/header-menu.svg";
     }
 });
 
 // Component 1 Swiper
-const swiper1 = new Swiper('.c1-swiper', {
+const swiper1 = new Swiper(".c1-swiper", {
     loop: true,
     pagination: {
         el: ".swiper-pagination",
-      },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        clickable: true,
     },
-    effect: "slide"
-  });
-
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    effect: "slide",
+});
 
 //   Component 3 Swiper
 let swiper2;
-function initSwiper() {
+function initC3Swiper() {
     const screenWidth = window.innerWidth;
     if (swiper2) {
         swiper2.destroy(true, true);
@@ -50,9 +50,39 @@ function initSwiper() {
     }
 }
 
+// Component 4 Swiper
+let swiper3;
+function initC5Swiper() {
+    const screenWidth = window.innerWidth;
+    if (swiper3) {
+        swiper3.destroy(true, true);
+    }
+
+    if (screenWidth < 576) {
+        swiper3 = new Swiper(".c5-swiper", {
+            slidesPerView: "auto",
+            centeredSlides: true,
+            spaceBetween: 10,
+        });
+    } else {
+        swiper3 = new Swiper(".c5-swiper", {
+            slidesPerView: 3,
+            centeredSlides: false,
+            spaceBetween: 30,
+            slidesPerGroup: 3,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    initSwiper();
+    initC3Swiper();
+    initC5Swiper();
     window.addEventListener("resize", () => {
-        initSwiper();
+        initC3Swiper();
+        initC5Swiper();
     });
 });
