510e6ee9ae696905cd8cc9ef5a55b9ccddaa9589
Artur Sultanov <artur_sultanov@sme-soft.by>
2024-12-05 17:41:52 +0300

    fix: mobile, desktop filters according to design, mobile breadcrumbs, some mobile styles

--- shop_category.js
+++ shop_category.js
@@ -19,11 +19,14 @@ const sort_btn = document.getElementById("sort-btn");
 const close_menu_btn = document.getElementById("close-menu");
 const close_sort_btn = document.getElementById("close-sort-btn");
 const return_btn = document.getElementById("return-btn");
+const product_type_return_btn = document.getElementById("product-type-return-btn");
+console.log(product_type_return_btn);
 const availability_btn = document.getElementById("availability-btn");
 const product_type_btn = document.getElementById("product-type-btn");
 
 const mobile_filter_menu = document.querySelector(".mobile-filter-menu");
 const mobile_availability_menu = document.querySelector(".mobile-availability-menu");
+const mobile_product_type_menu = document.querySelector(".mobile-product-type-menu");
 const mobile_sort_menu = document.querySelector(".mobile-sort-menu");
 
 filter_btn.addEventListener("click", () => {
@@ -38,9 +41,17 @@ availability_btn.addEventListener("click", () => {
     mobile_availability_menu.style.display = "block";
 });
 
+product_type_btn.addEventListener("click", () => {
+    mobile_product_type_menu.style.display = "block";
+});
+
 return_btn.addEventListener("click", () => {
     mobile_availability_menu.style.display = "none";
 });
+
+product_type_return_btn.addEventListener("click", () => {
+    mobile_product_type_menu.style.display = "none";
+})
 
 sort_btn.addEventListener("click", () => {
     mobile_sort_menu.style.display = "block";
